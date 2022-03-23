44f4457a2eaf326029acd33564a7360a6d623fa7
Shohid Al Morsalin <68673718+SAMorsalin@users.noreply.github.com>
2022-03-23 10:51:56 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,158 @@
+<?php
+session_start();
+?>
+
+<html>
+
+  <head>
+    <title> Home | BRUR-Cafeteria </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+
+  <link rel="stylesheet" type = "text/css" href ="css/index.css">
+
+  <body>
+
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">BRUR_Cafeteria</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li class="active" ><a href="index.php">Home</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+
+          </ul>
+
+<?php
+if(isset($_SESSION['login_user1'])){
+
+?>
+
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Hi <?php echo $_SESSION['login_user1']; ?> </a></li>
+            <li><a href="myrestaurant.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class=""></span> Log Out </a></li>
+          </ul>
+<?php
+}
+else if (isset($_SESSION['login_user2'])) {
+  ?>
+           <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class=""></span> Hi <?php echo $_SESSION['login_user2']; ?> </a></li>
+            <li><a href="foodlist.php"><span class=""></span> Food Zone </a></li>
+            <li><a href="cart.php"><span class=""></span> Cart
+              (<?php
+              if(isset($_SESSION["cart"])){
+              $count = count($_SESSION["cart"]); 
+              echo "$count"; 
+            }
+              else
+                echo "0";
+              ?>)
+             </a></li>
+            <li><a href="logout_u.php"><span class=""></span> Log Out </a></li>
+          </ul>
+  <?php        
+}
+else {
+
+  ?>
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class=""></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="customersignup.php"> User Sign-up</a></li>
+              <li> <a href="managersignup.php"> Manager Sign-up</a></li>
+              
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class=""></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a href="customerlogin.php"> User Login</a></li>
+              <li> <a href="managerlogin.php"> Manager Login</a></li>
+             
+            </ul>
+            </li>
+          </ul>
+
+<?php
+}
+?>
+       </div>
+
+      </div>
+    </nav>
+
+    <div class="">
+      	<div class="col-xs-5 line"><hr></div>
+        <div class="col-xs-2 logo"><img src="images/logo.png" style="width: 120px"></div>
+        <div class="col-xs-5 line"><hr></div>
+        <div class="tagline"> 
+        <h1 style="font-family:Bradley Hand, cursive; color: red; margin-bottom: 25px; text-align: center; font-size: 50px;"> Good Food is Good Mood </h1>
+          
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="orderblock">
+    <h2>Feeling Hungry?</h2>
+    <center><a class="btn btn-success btn-lg" href="customerlogin.php" role="button" > Order Now </a></center>
+    </div>
+
+    
+  
+</body>
+<br>
+<br>
+<footer class="container-fluid bg-4 text-center">
+  <br>
+  <p> Food Exploria 2021 | Developed by Morsalin, Sumon & Bappy </p>
+  <br>
+</footer>
+</html>
